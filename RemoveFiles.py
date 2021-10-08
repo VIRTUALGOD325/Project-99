06a4c36d2b379f539c64a42f54aca7e47d69ab67
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2021-10-08 22:13:46 +0530

    Add files via upload

--- RemoveFiles.py
+++ RemoveFiles.py
@@ -1,14 +1,49 @@
 import os
 import shutil
+import time
 
 
-source = input("Enter The Folder Name: ")
-destination = input('Enter Destination Folder Name: ')
+def main():
+	deleted_folders_count = 0
+	deleted_files_count = 0
+	path = "/PATH_TO_DELETE"
+	days = 30
+	seconds = time.time() - (days * 24 * 60 * 60)
+	if os.path.exists(path):
+		for root_folder, folders, files in os.walk(path):
+			if seconds >= get_file_or_folder_age(root_folder):
+				deleted_folders_count += 1 
+				break
+			else:
+				for folder in folders:
+					folder_path = os.path.join(root_folder, folder)
+					if seconds >= get_file_or_folder_age(folder_path):
+						deleted_folders_count += 1
 
-source = source + '/'
-destination = destination + '/'
+
+				for file in files:
+					file_path = os.path.join(root_folder, file)
+					if seconds >= get_file_or_folder_age(file_path):
+						deleted_files_count += 1 
+
+		else:
+			if seconds >= get_file_or_folder_age(path):				
+				deleted_files_count += 1 
+
+	else:
+		print(f'"{path}" is not found')
+		deleted_files_count += 1 
+
+	print(f"Total folders deleted: {deleted_folders_count}")
+	print(f"Total files deleted: {deleted_files_count}")
 
 
-list_of_files = os.listdir(source)
-for file in list_of_files:
-    shutil.move((source+file), destination)
+
+def get_file_or_folder_age(path):
+
+	ctime = os.stat(path).st_ctime
+	return ctime
+
+
+if __name__ == '__main__':
+	main()
